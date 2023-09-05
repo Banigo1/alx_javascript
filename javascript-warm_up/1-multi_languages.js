0bd4043ff0c9aefb5336971160c16c8c6737c6c7
Precious Banigo <banigoprecious@yahoo.com>
2023-09-05 07:52:48 +0100

    JavaScript - Warm up

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-const lines = ["C is fun", "Pyhron is cool", "Javascript is amazing"];
+const lines = ["C is fun", "Pyhron is cool", "JavaScript is amazing"];
 for(let i = 0; i < lines.lenght; 1++){
     console.log(lines[i]);
 }
