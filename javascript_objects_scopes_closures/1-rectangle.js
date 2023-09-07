798ec43c5a40047435999249d6df266336e14d56
Precious Banigo <banigoprecious@yahoo.com>
2023-09-07 11:19:49 +0100

    ...

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+
+/*class Rectangle that defines a rectangle
+class notation for defining the class
+The constructor takes 2 arguments w and h
+Instance Initialization attribute width with the value of w
+Instance Initialization attribute height with the value of h*/
+module.exports = class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  };
