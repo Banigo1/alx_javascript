ff594394e8e7142277fe4b17de474876c89620f7
Precious Banigo <banigoprecious@yahoo.com>
2023-09-19 13:14:18 +0100

    Star wars API

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,23 +1,38 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-
-request(url + movieId, (error, response, body) => {
+function fetchMovieCharacters(movieId) {
+  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+  
+  request.get(apiUrl, (error, response, body) => {
     if (error) {
-        console.log(error);
-        return;
-    }
-    const movies = JSON.parse(body);
+      console.error('Error:', error);
+    } else {
+      if (response.statusCode === 200) {
+        const movieData = JSON.parse(body);
+        const characters = movieData.characters;
 
-    for (const movie of movies.characters) {
-        request.get(movie, (error, response, body1) => {
-            if (error) {
-                console.log(error);
+        console.log(`Characters in Star Wars Episode ${movieData.episode_id}: ${movieData.title}`);
+        console.log('---------------------------');
+
+        characters.forEach((characterUrl) => {
+          request.get(characterUrl, (charError, charResponse, charBody) => {
+            if (charError) {
+              console.error('Error:', charError);
+            } else {
+              if (charResponse.statusCode === 200) {
+                const characterData = JSON.parse(charBody);
+                console.log(characterData.name);
+              }
             }
-            const movieData = JSON.parse(body1);
-            console.log(movieData.name);
+          });
         });
+      } else {
+        console.error('Failed to fetch movie data. Status code:', response.statusCode);
+      }
     }
-});
+  });
+}
+
+const movieId = 3; // Replace with the desired movie ID
+fetchMovieCharacters(movieId);
