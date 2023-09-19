f5258a226971a73791d4cbe8d1d4adef20fa9ac4
Precious Banigo <banigoprecious@yahoo.com>
2023-09-19 11:40:55 +0100

    ...

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,19 +1,14 @@
-// Import the 'request' module
 const request = require('request');
 
-// Function to make a GET request and display the status code
-function getRequestStatusCode(url){
-  request.get(url, (error, response, body) =>
-  {
-    if (error){
-      console.error('Error:', error);
-    }
-    else {
-      console.log('Status code:', response.statusCode);
+function getRequestStatusCode(url) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`code: ${response.statusCode}`);
     }
   });
 }
 
-// Example usage
 const urlToRequest = 'https://example.com'; // Replace with your desired URL
 getRequestStatusCode(urlToRequest);
