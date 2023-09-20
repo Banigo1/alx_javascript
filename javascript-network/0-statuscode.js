959f8ea507a109fc7a2d8580a1d690578f32e031
Precious Banigo <banigoprecious@yahoo.com>
2023-09-20 16:59:55 +0100

    ...

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,7 +3,9 @@ const request = require('request');
 let url = process.argv[2];
 
 request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log('code:', response && response.statusCode);
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    console.log('code:', response.statusCode);
   }
 });
